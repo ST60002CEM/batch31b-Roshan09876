f73284b1f218263a74b15eb1580d445eb86ac95a
Roshan Kumar Khadka <roshankumar@Roshans-MacBook-Air.local>
2023-10-30 16:33:48 +0545

    SignUp page

---
+++ lib/view/dashboard_view.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 196, 238, 240),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft, // Corrected alignment
+              child: CircleAvatar(
+                radius: 70,
+                backgroundColor: Color.fromARGB(255, 158, 204, 242),
+              ),
+            ),
+            SizedBox(height: 70),
+            Text(
+              'Hello, Welcome!',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              'Fill the below fields for SignUp',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.text_increase_outlined,
+                    color: Colors.black,
+                  ),
+                  labelText: 'Full Name',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8))),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                  ),
+                  labelText: 'E-mail',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.password,
+                    color: Colors.black,
+                  ),
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  suffixIcon: Icon(
+                    Icons.visibility_off,
+                    color: Colors.black,
+                  )),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(200, 60),
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                onPressed: () {},
+                child: Text(
+                  'SignUp',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Already have an account? Login',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
