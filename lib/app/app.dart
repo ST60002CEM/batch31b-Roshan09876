a20cf73cda1a7e51e68e08edbff02dadc859fafc
Roshan Kumar Khadka <roshankumar@Roshans-MacBook-Air.local>
2023-12-03 21:49:58 +0545

    final login commit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:job_finder/app/routes/app_routes.dart';
 import 'package:job_finder/view/login_view.dart';
-import 'package:job_finder/view/theme/themedata.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +9,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: getApplicationTheme(),
       home: LoginView(),
       initialRoute: AppRoute.registerRoute,
       routes: AppRoute.getApplicationRoute(),
