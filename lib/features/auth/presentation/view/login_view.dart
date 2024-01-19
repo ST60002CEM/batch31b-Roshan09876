100209e79ad3306e4cb4bb3cc7d1865a433828a7
Roshan Kumar Khadka <roshankumar@Roshans-MacBook-Air.local>
2024-01-19 10:56:13 +0545

    running in real ios

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -37,121 +37,123 @@ class _LoginViewState extends State<LoginView> {
             padding: const EdgeInsets.all(10.0),
             child: Form(
               key: key,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Container(
-                      height: 330,
-                      width: 430,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(200),
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/sigin.png'),
-                          )),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Container(
+                        height: 330,
+                        width: 430,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(200),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/sigin.png'),
+                            )),
+                      ),
                     ),
-                  ),
-                  _gap,
-                  ReusableText(
-                      text: 'Welcome!',
-                      fontSize: 40,
-                      color: Color(kDark.value)),
-                  _gap,
-                  ReusableText(
-                      text: 'Fill the below fields for Sign In',
-                      fontSize: 25,
-                      color: Color(kDark.value)),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    controller: emailcontroller,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.email,
-                          color: Color(kDark.value),
-                        ),
-                        labelText: 'Email',
-                        border: OutlineInputBorder()),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please Enter you email';
-                      } else if (!value.contains('@')) {
-                        return '@ is missing';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    obscureText: _obsecuretext,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.password,
-                          color: Color(kDark.value),
-                        ),
-                        labelText: 'Password',
-                        border: OutlineInputBorder(),
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _obsecuretext = !_obsecuretext;
-                            });
-                          },
-                          child: Icon(
-                            _obsecuretext
-                                ? Icons.visibility
-                                : Icons.visibility_off,
+                    _gap,
+                    ReusableText(
+                        text: 'Welcome!',
+                        fontSize: 40,
+                        color: Color(kDark.value)),
+                    _gap,
+                    ReusableText(
+                        text: 'Fill the below fields for Sign In',
+                        fontSize: 25,
+                        color: Color(kDark.value)),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      controller: emailcontroller,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
                             color: Color(kDark.value),
                           ),
+                          labelText: 'Email',
+                          border: OutlineInputBorder()),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please Enter you email';
+                        } else if (!value.contains('@')) {
+                          return '@ is missing';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      obscureText: _obsecuretext,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.password,
+                            color: Color(kDark.value),
+                          ),
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _obsecuretext = !_obsecuretext;
+                              });
+                            },
+                            child: Icon(
+                              _obsecuretext
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Color(kDark.value),
+                            ),
+                          )),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please Enter Your password';
+                        } else if (value.length < 6) {
+                          return 'Passwors must contai six letters';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(kDarkPurple.value),
+                            minimumSize: Size(200, 60)),
+                        onPressed: () {
+                          if (key.currentState!.validate()) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => RegisterView()));
+                          }
+                        },
+                        child: ReusableText(
+                          color: Color(kLight.value),
+                          fontSize: 20,
+                          text: 'Sign In',
                         )),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please Enter Your password';
-                      } else if (value.length < 6) {
-                        return 'Passwors must contai six letters';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(kDarkPurple.value),
-                          minimumSize: Size(200, 60)),
-                      onPressed: () {
-                        if (key.currentState!.validate()) {
+                    SizedBox(
+                      height: 15,
+                    ),
+                    InkWell(
+                        onTap: () {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
                                   builder: (context) => RegisterView()));
-                        }
-                      },
-                      child: ReusableText(
-                        color: Color(kLight.value),
-                        fontSize: 20,
-                        text: 'Sign In',
-                      )),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegisterView()));
-                      },
-                      child: ReusableText(
-                        text: "Don't have an account? Sign Up",
-                        fontSize: 15,
-                        color: Color(kDark.value),
-                      )),
-                ],
+                        },
+                        child: ReusableText(
+                          text: "Don't have an account? Sign Up",
+                          fontSize: 15,
+                          color: Color(kDark.value),
+                        )),
+                  ],
+                ),
               ),
             ),
           )),
